23a2c114c1215da09e7523e6a5b26d43f5e2063d
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-18 19:21:15 -0700

    Setup webpack and file structure

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => res.send('Hello World'));
+const path = require('path');
 
 app.listen(4001, () => console.log('App connected on port 4001'));
+
+// app.get('/', (req, res) => res.send('WTF'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+console.log(path.join(__dirname, '../client/dist'));
+
