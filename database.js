b4d62fd1b41fd05ea0e36dfed7f22ddac842192c
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-23 11:42:54 -0700

    Populate the mongoDB with song entries

--- database.js
+++ database.js
@@ -1,23 +1,37 @@
+const data = require('./randomDataValGen.js');
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/streamboard');
+mongoose.connect('mongodb://localhost/suggestedTracks');
 
 const db = mongoose.connection;
 db.on('error', () => console.log('error connecting to mongo'));
 db.once('open', () => console.log('has conected to mongo'));
 
-var trackSchema = mongoose.Schema({
-  SongId: Number,
-  SongName: String,
-  Artist: String,
-  Album: String,
-  Genre: String,
-  Plays: Number,
-  Likes: Number,
-  Shares: Number,
-  Comments: Number
+const trackSchema = mongoose.Schema({
+  id: Number,
+  title: String,
+  artist: String,
+  genre: String,
+  album: String,
+  plays: Number,
+  likes: Number,
+  shares: Number,
+  comments: Number,
 });
 
-var Track = mongoose.model('Track', trackSchema);
+const Track = mongoose.model('Track', trackSchema);
+// console.log(data.sampleData)
 
+for (let i = 0; i < data.sampleData.length; i += 1) {
+  const trackDocument = new Track(data.sampleData[i]);
+  console.log(trackDocument);
+  trackDocument.save((error, data) => {
+    if (error) {
+      console.log('error saving document');
+    } else {
+      console.log('document successfully saved', data);
+    }
+  });
+}
 //need to crete a new document for each track
 //pass each track object as the the property list to Track constructor
