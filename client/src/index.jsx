c4e09fd61430f21249a828dab545c82020e7e474
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-25 17:25:25 -0700

    Rename index.js to index.jsx and successfully render app component

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SuggestedTrackList from './components/SuggestedTrackList.jsx';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div> Mydiv </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
