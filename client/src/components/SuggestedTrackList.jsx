4747deeb5733abd89b6902d9610d7f66300d6aa8
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-26 12:41:42 -0700

    Add horizontal rule and space after icon

--- client/src/components/SuggestedTrackList.jsx
+++ client/src/components/SuggestedTrackList.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import styledComponents from 'styled-components';
-const SuggestedTrackBox = styledComponents.div`
+import styled from 'styled-components';
+const SuggestedTrackBox = styled.div`
   margin: 2px;
   border: 1px solid lightgray;
+`;
+
+const HorizontalLine = styled.hr`
+  width: 300px;
+  color: lightgray;
 `;
 
 const SuggestedTrackList = (props) => {
@@ -10,10 +15,10 @@ const SuggestedTrackList = (props) => {
     <SuggestedTrackBox>
     <div className = 'suggested-track-list'>
       <h3>
-            <i className="fas fa-music" />
+            <i className="fas fa-music">&nbsp;&nbsp;</i>
             Related Tracks
           </h3>
-          <hr/>
+          <HorizontalLine/>
       <div>
         <h2> 
         suggestedtrack entry goes here 
