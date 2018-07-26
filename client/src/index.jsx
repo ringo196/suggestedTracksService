157b28a1584b038efef9bd0abd32baac0588378a
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-26 00:22:04 -0700

    Use styled components to add basic style to the SuggestedTrackList component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styledComponents from 'styled-components';
 import SuggestedTrackList from './components/SuggestedTrackList.jsx';
+
+const SuggestedTrackBox = styledComponents.div`
+  margin: 2px;
+  border: 1px solid lightgray;
+`;
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-      <div> Mydiv will now render this</div>
-      <SuggestedTrackList />
+      <SuggestedTrackBox>
+        <div>
+          <h3>
+            <i className="fas fa-music" />
+            Related Tracks
+          </h3>
+          <hr/>
+        </div>
+      <div> 
+        Mydiv will now render this
       </div>
+      <SuggestedTrackList />
+      </SuggestedTrackBox>
     );
   }
 }
