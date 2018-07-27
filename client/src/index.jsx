aba47b7716b332b9a0ee6acb6fd9578b6a6e9437
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-27 00:35:09 -0700

    Setup basic AJAX requests to server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styledComponents from 'styled-components';
+import $ from 'jquery';
 import SuggestedTrackList from './components/SuggestedTrackList.jsx';
 
 const SuggestedTrackBox = styledComponents.div`
@@ -9,11 +10,39 @@ const SuggestedTrackBox = styledComponents.div`
 `;
 
 class App extends React.Component {
+
+  loadInitialData() {
+    let context = this;
+    $.ajax('/songs/:id/suggestedTracks', {
+      method: 'GET',
+      error: (error) => {
+        console.log('error with getting data', error);
+      },
+      success: (data) => {
+        console.log('successfully got', data);
+        context.setState({ suggestedTracks: data });
+      },
+    });
+  }
+
+  incrementMetric() {
+    let context = this;
+    $.ajax('/suggestedTracks/:id/:category', {
+      method: 'PUT',
+      error: (error) => {
+        console.log('error with incrementing data', error);
+      },
+      success: (data) => {
+        console.log('successfully got', data);
+      },
+    });
+  }
+
   render() {
     return (
       <div>
-    <SuggestedTrackList />
-    </div>
+        <SuggestedTrackList />
+      </div>
     );
   }
 }
