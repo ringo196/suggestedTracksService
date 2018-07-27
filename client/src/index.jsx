00595ebc4dc9afb48919d506c1734312800aebc6
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-27 10:47:20 -0700

    Use ajax to get track1 upon mounting. Make function to choose three random suggested tracks for that track

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,23 +4,30 @@ import styledComponents from 'styled-components';
 import $ from 'jquery';
 import SuggestedTrackList from './components/SuggestedTrackList.jsx';
 
-const SuggestedTrackBox = styledComponents.div`
-  margin: 2px;
-  border: 1px solid lightgray;
-`;
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  loadInitialData() {
+    this.state = {
+      currentTrack: '',
+      suggestedTracks: [],
+      threeSuggestions: [],
+    }
+  }
+
+  componentDidMount() {
     let context = this;
-    $.ajax('/songs/:id/suggestedTracks', {
+    $.ajax('/songs/1/suggestedTracks', {
       method: 'GET',
       error: (error) => {
         console.log('error with getting data', error);
       },
       success: (data) => {
         console.log('successfully got', data);
-        context.setState({ suggestedTracks: data });
+        context.setState({ suggestedTracks: data }, () => {
+          console.log(context.state.suggestedTracks);
+          this.displayThreeSuggestions();
+        });
       },
     });
   }
@@ -35,6 +42,22 @@ class App extends React.Component {
       success: (data) => {
         console.log('successfully got', data);
       },
+    });
+  }
+
+  displayThreeSuggestions() {
+    let arrayOfTracks = this.state.suggestedTracks.slice();
+    let randomTracks = [];
+    let x = 0;
+    while (x < 3) {
+      let randomIndex = Math.floor(Math.random() * (arrayOfTracks.length - 1) + 1);
+      console.log(randomIndex);
+      randomTracks.push(arrayOfTracks[randomIndex]);
+      arrayOfTracks.splice(randomIndex, 1);
+      x += 1;
+    }
+    this.setState({ threeSuggestions: randomTracks }, () => {
+      console.log(this.state.threeSuggestions);
     });
   }
 
